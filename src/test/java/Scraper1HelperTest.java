a49d5a19d43f9e73d9e99ae060218476269d683f
David Adediji <davidadey000@gmail.com>
2023-12-15 09:19:40 +0100

    Minor improvements to parser real-time

--- src/test/java/Scraper1HelperTest.java
+++ src/test/java/Scraper1HelperTest.java
@@ -31,6 +31,7 @@ class Scraper1HelperTest {
         String htmlContent = readHtmlFromFile(HTML_FILE_PATH);
         Document document = Jsoup.parse(htmlContent);
         String description = Scraper1Helper.extractProductDescription(document);
+        System.out.println(description);
         assertEquals("The iPhone 11 features a 6.1-inch LCD display that Apple calls a \"Liquid Retina HD Display.\" It features a 1792 x 828 resolution at 326ppi, a 1400:1 contrast ratio, 625 nits max brightness, True Tone support for adjusting the white balance to the ambient lighting, and wide color support for true-to-life colors. The iPhone 11 is available in six different colors: White, Black, Yellow, (PRODUCT)RED, Purple, and Green.", description);
     }
 
@@ -38,7 +39,8 @@ class Scraper1HelperTest {
     void testExtractProductPrice() throws IOException {
         Document document = Jsoup.parse(readHtmlFromFile(HTML_FILE_PATH));
         Price price = Scraper1Helper.extractProductPrice(document);
-        assertEquals(259.00, price.getAmount());
+        System.out.println(price.getCurrency()+price.getAmount());
+        assertEquals(262.00, price.getAmount());
         assertEquals("$", price.getCurrency());
     }
 
@@ -46,6 +48,7 @@ class Scraper1HelperTest {
     void testExtractProductImageUrl() throws IOException {
         Document document = Jsoup.parse(readHtmlFromFile(HTML_FILE_PATH));
         String imageUrl = Scraper1Helper.extractProductImageUrl(document);
+        System.out.println(imageUrl);
         // Replace "YourExpectedImageUrl" with the expected URL
         assertEquals("https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/61MG3m5FhIL.__AC_SX300_SY300_QL70_FMwebp_.jpg", imageUrl);
     }
@@ -54,6 +57,7 @@ class Scraper1HelperTest {
     void testExtractProductManufacturer() throws IOException {
         Document document = Jsoup.parse(readHtmlFromFile(HTML_FILE_PATH));
         String manufacturer = Scraper1Helper.extractProductManufacturer(document);
+        System.out.println(manufacturer);
         assertEquals("Apple", manufacturer);
     }
     // Add tests for variations and properties as needed...
